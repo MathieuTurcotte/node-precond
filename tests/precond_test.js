9beaeaa8fba15e2afa638a599a2f0af7bd2d75a8
Mathieu Turcotte <turcotte.mat@gmail.com>
2013-06-01 12:02:36 -0400

    Add default error messages for type checks.
    Closes #1.

--- tests/precond_test.js
+++ tests/precond_test.js
@@ -28,6 +28,9 @@ exports["Precond"] = {
         test.throws(function() {
             precond.checkIsDef(undefined, 'abc %s def %s', 1, 3);
         }, /abc 1 def 3/);
+        test.throws(function() {
+            precond.checkIsDef(undefined);
+        }, /Expected value to be defined but was undefined./);
         test.equals(0, precond.checkIsDef(0));
         test.done();
     },
@@ -42,17 +45,22 @@ exports["Precond"] = {
         test.throws(function() {
             precond.checkIsDefAndNotNull(null, 'abc %s def %s', 1, 3);
         }, /abc 1 def 3/);
+        test.throws(function() {
+            precond.checkIsDefAndNotNull(null);
+        }, /Expected value to be defined and not null but got "null"./);
         test.equals(0, precond.checkIsDefAndNotNull(0));
         test.done();
     },
 
     "checkIsString": function(test) {
         test.doesNotThrow(function() { precond.checkIsString(''); });
-        test.doesNotThrow(function() { precond.checkIsString(String('')); });
-        test.doesNotThrow(function() { precond.checkIsString(new String('')); });
+        test.throws(function() { precond.checkIsString(new String('')); });
         test.throws(function() {
-            precond.checkIsString({}, 'fake error string');
-        }, /fake error string/);
+            precond.checkIsString({}, 'abc %s def %s', 1, 3);
+        }, /abc 1 def 3/);
+        test.throws(function() {
+            precond.checkIsString({});
+        }, /Expected "string" but got "object"./);
         test.equals('foo', precond.checkIsString('foo'));
         test.done();
     },
@@ -62,6 +70,9 @@ exports["Precond"] = {
         test.throws(function() {
             precond.checkIsArray({}, 'abc %s def %s', 1, 3);
         }, /abc 1 def 3/);
+        test.throws(function() {
+            precond.checkIsArray({});
+        }, /Expected "array" but got "object"./);
         var arr = [1, 2];
         test.equals(arr, precond.checkIsArray(arr));
         test.done();
@@ -72,6 +83,9 @@ exports["Precond"] = {
         test.throws(function() {
             precond.checkIsNumber({}, 'abc %s def %s', 1, 3);
         }, /abc 1 def 3/);
+        test.throws(function() {
+            precond.checkIsNumber({});
+        }, /Expected "number" but got "object"./);
         test.equals(0, precond.checkIsNumber(0));
         test.done();
     },
@@ -82,6 +96,9 @@ exports["Precond"] = {
         test.throws(function() {
             precond.checkIsBoolean({}, 'abc %s def %s', 1, 3);
         }, /abc 1 def 3/);
+        test.throws(function() {
+            precond.checkIsBoolean({});
+        }, /Expected "boolean" but got "object"./);
         test.equals(true, precond.checkIsBoolean(true));
         test.done();
     },
@@ -89,8 +106,11 @@ exports["Precond"] = {
     "checkIsFunction": function(test) {
         test.doesNotThrow(function() { precond.checkIsFunction(function() {}); });
         test.throws(function() {
-            precond.checkIsBoolean({}, 'abc %s def %s', 1, 3);
+            precond.checkIsFunction({}, 'abc %s def %s', 1, 3);
         }, /abc 1 def 3/);
+        test.throws(function() {
+            precond.checkIsFunction({});
+        }, /Expected "function" but got "object"./);
         var fn = function() {};
         test.equals(fn, precond.checkIsFunction(fn));
         test.done();
@@ -104,6 +124,9 @@ exports["Precond"] = {
         test.throws(function() {
             precond.checkIsObject(null, 'abc %s def %s', 1, 3);
         }, /abc 1 def 3/);
+        test.throws(function() {
+            precond.checkIsObject(null);
+        }, /Expected "object" but got "null"./);
         var obj = {};
         test.equals(obj, precond.checkIsObject(obj));
         test.done();
